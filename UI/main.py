f0c34e18e623bd33f9d1226c495bf9a51b8768ce
Roselle20 <roselledurano14@gmail.com>
2025-04-27 19:34:54 +0800

    Add files via upload

---
+++ UI/main.py
@@ -0,0 +1,23 @@
+import sys
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QLabel, QLineEdit,
+                             QPushButton, QGridLayout, QVBoxLayout)
+from PyQt5.QtGui import QFont
+
+from login import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    
+    windows = MainWindow()
+    windows.show()
+    
+    sys.exit(app.exec())
+    
+    
